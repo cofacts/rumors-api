928953ed9f145025dcf2930667005ee823eb6114
MrOrz <johnsonliang7@gmail.com>
2016-12-16 22:49:34 +0800

    Use apollo server instead, and add an index that is more informative.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import config from 'config';
-import graphqlHTTP from 'koa-graphql-next';
+import path from 'path';
+import pug from 'pug';
+import { printSchema } from 'graphql/utilities';
+import { graphqlKoa, graphiqlKoa } from 'graphql-server-koa';
+import koaStatic from 'koa-static';
+import koaBody from 'koa-bodyparser';
 import schema from './graphql/schema';
 import DataLoaders from './graphql/dataLoaders';
 
 const app = new Koa();
+const router = Router();
 
-app.use((ctx, next) => graphqlHTTP({
+app.use(koaBody());
+
+router.post('/graphql', graphqlKoa(() => ({
   schema,
-  graphiql: true,
   context: {
     loaders: new DataLoaders(), // new loaders per request
   },
-})(ctx, next));
+})));
 
-const router = Router();
+router.get('/graphql', graphiqlKoa({ endpointURL: '/graphql' }));
 
+const indexFn = pug.compileFile(path.join(__dirname, 'jade/index.jade'));
+const schemaStr = printSchema(schema);
 router.get('/', (ctx) => {
-  ctx.body = 'Hello Koa';
+  ctx.body = indexFn({ schemaStr });
 });
 
+app.use(koaStatic(path.join(__dirname, '../static/')));
 app.use(router.routes());
 app.use(router.allowedMethods());
 
