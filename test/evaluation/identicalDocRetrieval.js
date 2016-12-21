92c074f524699c61c7f029e898182d7d6a231f05
MrOrz <johnsonliang7@gmail.com>
2016-12-22 01:29:11 +0800

    [evaluation] better progress report

--- test/evaluation/identicalDocRetrieval.js
+++ test/evaluation/identicalDocRetrieval.js
@@ -1,7 +1,10 @@
+/* eslint no-console: off */
+
 import 'util/catchUnhandledRejection';
 import client, { processMeta } from 'util/client';
 import getIn from 'util/getInFactory';
 import GraphQL from '../util/GraphQL';
+import Progress from 'progress';
 
 async function main() {
   const allRumors = getIn(await client.search({
@@ -11,49 +14,55 @@ async function main() {
     },
   }))(['hits', 'hits'], []).map(processMeta);
 
-  let invalidCount = 0;
-  for (const { id, text } of allRumors) {
-    // perform graphql search...
-    const { data } = await GraphQL({
-      query: `
-        query ($text: String) {
-          Search(text: $text, findInCrawled: false) {
-            rumors {
-              score
-              doc {
-                id
-                text
-                answers {
-                  versions {
-                    text
-                  }
+  console.log('Querying rumors in DB...');
+  const progress = new Progress('[:bar] :current/:total :etas', { total: allRumors.length });
+
+  const allResults = await Promise.all(allRumors.map(({ text }) => GraphQL({
+    query: `
+      query ($text: String) {
+        Search(text: $text, findInCrawled: false) {
+          rumors {
+            score
+            doc {
+              id
+              text
+              answers {
+                versions {
+                  text
                 }
               }
             }
-            suggestedResult {
-              __typename
-              ... on Rumor {
-                id
-                text
-              }
+          }
+          suggestedResult {
+            __typename
+            ... on Rumor {
+              id
+              text
             }
           }
         }
-      `,
-      variables: {
-        text,
-      },
-    });
+      }
+    `,
+    variables: {
+      text,
+    },
+  }).then((data) => {
+    progress.tick();
+    return data;
+  })));
 
-    if (data.Search.suggestedResult === null || data.Search.suggestedResult.id !== id) {
+  let invalidCount = 0;
+  allResults.forEach(({ data }, i) => {
+    const rumor = allRumors[i];
+    if (data.Search.suggestedResult === null || data.Search.suggestedResult.id !== rumor.id) {
       invalidCount += 1;
-      console.log(id, data.Search.suggestedResult && data.Search.suggestedResult.id);
+      console.log(rumor.id, data.Search.suggestedResult && data.Search.suggestedResult.id);
     }
-  }
+  });
 
-  console.log("=========");
-  console.log(`${allRumors.length - invalidCount}/${allRumors.length} correct`)
-  console.log(`${(100*(allRumors.length - invalidCount)/allRumors.length).toFixed(2)} % correct.`)
+  console.log('---- Summary ----');
+  console.log(`${allRumors.length - invalidCount}/${allRumors.length} correct`);
+  console.log(`${(100 * ((allRumors.length - invalidCount) / allRumors.length)).toFixed(2)} % correct.`);
 }
 
 main();
