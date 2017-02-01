7723444ab607a6721679ee7594e63dccda44206b
MrOrz <johnsonliang7@gmail.com>
2017-02-02 00:08:26 +0800

    Merge commit '436f1cc0a7998a9a51ecdb79c8e595b4b198cd81’. Fixes #24 .

--- test/rumors-db/schema/replyrequests.js
+++ test/rumors-db/schema/replyrequests.js
@@ -5,8 +5,15 @@ export default {
   _all: { enabled: false },
   properties: {
     articleId: { type: 'keyword' },
+
+    // only recognizable for within a client.
+    //
     userId: { type: 'keyword' },
+
+    // The user submits the request with which client.
+    // Should be something like API-key in the future.
     from: { type: 'keyword' },
+
     createdAt: { type: 'date' },
     updatedAt: { type: 'date' },
   },
