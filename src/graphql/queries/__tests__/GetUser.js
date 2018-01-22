235829594d34380cbeb77466137a8ac6db36b944
MrOrz <johnsonliang7@gmail.com>
2018-01-22 20:38:18 +0800

    Fix GetUser fixtures

--- src/graphql/queries/__tests__/GetUser.js
+++ src/graphql/queries/__tests__/GetUser.js
@@ -2,29 +2,33 @@ import gql from 'util/GraphQL';
 import { loadFixtures, unloadFixtures } from 'util/fixtures';
 import fixtures from '../__fixtures__/GetUser';
 
-const currentUser = fixtures['/users/basic/current-user'];
+const currentUser = fixtures['/users/doc/current-user'];
 describe('GetUser', () => {
   beforeAll(() => loadFixtures(fixtures));
 
   it('Get current user specified in context when no ID is given', async () => {
     expect(
-      await gql`{
-      GetUser {
-        name
-        email
-      }
-    }`({}, { user: currentUser })
+      await gql`
+        {
+          GetUser {
+            name
+            email
+          }
+        }
+      `({}, { user: currentUser })
     ).toMatchSnapshot();
   });
 
   it('Get limited data', async () => {
     expect(
-      await gql`{
-      GetUser(id: "test-user") {
-        name
-        email
-      }
-    }`({}, { user: currentUser })
+      await gql`
+        {
+          GetUser(id: "test-user") {
+            name
+            email
+          }
+        }
+      `({}, { user: currentUser })
     ).toMatchSnapshot();
   });
 
