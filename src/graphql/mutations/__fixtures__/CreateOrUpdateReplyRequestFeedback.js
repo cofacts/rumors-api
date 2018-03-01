a27e572fa78f7472d4db827fe1b7a464d6207ea6
MrOrz <johnsonliang7@gmail.com>
2018-03-01 23:51:55 +0100

    Implement CreateOrUpdateReplyRequestFeedback API

---
+++ src/graphql/mutations/__fixtures__/CreateOrUpdateReplyRequestFeedback.js
@@ -0,0 +1,21 @@
+export default {
+  '/replyrequests/doc/foo': {
+    articleId: 'foo-article',
+    userId: 'user',
+    appId: 'app',
+    reason: 'foo-reason',
+    feedbacks: [
+      {
+        userId: 'fb-user-1',
+        appId: 'app',
+        score: 1,
+        createdAt: '2017-01-01T00:00:00.000Z',
+        updatedAt: '2017-01-01T00:00:00.000Z',
+      },
+    ],
+    positiveFeedbackCount: 1,
+    negativeFeedbackCount: 0,
+    createdAt: '2017-01-01T00:00:00.000Z',
+    updatedAt: '2017-01-01T00:00:00.000Z',
+  },
+};
