596b571fa74f77137ba395b77ae0d1524eb01813
Po-Cheng Chu <darkbtf@gmail.com>
2018-04-13 21:21:27 +0800

    update user name mutation

---
+++ src/graphql/mutations/UpdateUser.js
@@ -0,0 +1,33 @@
+import { GraphQLString, GraphQLNonNull } from 'graphql';
+import client from 'util/client';
+import User from 'graphql/models/User';
+
+export default {
+  type: User,
+  description: 'Change attribute of a user',
+  args: {
+    userId: { type: new GraphQLNonNull(GraphQLString) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+  },
+  async resolve(rootValue, { userId, name }) {
+    const { result, get: { _source } } = await client.update({
+      index: 'users',
+      type: 'doc',
+      id: userId,
+      body: {
+        doc: {
+          name,
+        },
+        _source: true,
+      },
+    });
+
+    if (result === 'noop') {
+      throw new Error(
+        `Cannot change name for user(userId=${userId})`
+      );
+    }
+
+    return _source;
+  },
+};
