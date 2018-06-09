460d46183540dc74ad6ece03e77adde99622436a
MrOrz <johnsonliang7@gmail.com>
2018-06-09 22:27:21 +0800

    Handle legacy replyRequests, they don't have feedbacks

--- src/graphql/models/ReplyRequest.js
+++ src/graphql/models/ReplyRequest.js
@@ -9,19 +9,19 @@ export default new GraphQLObjectType({
     reason: { type: GraphQLString },
     feedbackCount: {
       type: GraphQLInt,
-      resolve({ feedbacks }) {
+      resolve({ feedbacks = [] }) {
         return feedbacks.length;
       },
     },
     positiveFeedbackCount: {
       type: GraphQLInt,
-      resolve({ feedbacks }) {
+      resolve({ feedbacks = [] }) {
         return feedbacks.filter(fb => fb.score === 1).length;
       },
     },
     negativeFeedbackCount: {
       type: GraphQLInt,
-      resolve({ feedbacks }) {
+      resolve({ feedbacks = [] }) {
         return feedbacks.filter(fb => fb.score === -1).length;
       },
     },
--- src/graphql/queries/__fixtures__/GetReplyAndArticle.js
+++ src/graphql/queries/__fixtures__/GetReplyAndArticle.js
@@ -72,6 +72,11 @@ export default {
     reason: 'Reason foo',
     feedbacks: [{ score: 1 }, { score: -1 }],
   },
+  '/replyrequests/doc/articleTest2': { // Legacy reply request that has no feedbacks[] nor reason.
+    articleId: 'foo',
+    userId: 'fakeUser',
+    appId: 'LINE',
+  },
   [`/articlereplyfeedbacks/doc/${getArticleReplyFeedbackId({
     articleId: 'foo',
     replyId: 'bar',
