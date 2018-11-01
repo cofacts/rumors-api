a2d9fb2a6a2514cbea2e33e86937cc1a0394a449
MrOrz <johnsonliang7@gmail.com>
2018-11-01 22:59:53 +0800

    Separate text-only query and url-only query in moreLikeThis filter test

--- src/graphql/queries/__tests__/ListArticles.js
+++ src/graphql/queries/__tests__/ListArticles.js
@@ -126,10 +126,10 @@ describe('ListArticles', () => {
     ).toMatchSnapshot();
   });
 
-  it('filters by moreLikeThis and handles URLs', async () => {
+  it('filters by moreLikeThis and given text, find articles containing hyperlinks with the said text', async () => {
     // moreLikeThis query
-    // 1, given text, find articles containing hyperlinks with the said text
-    // 2. given URL, find articles with content the said URL's content
+    // 1,
+    // 2.
     expect(
       await gql`
         query($like: String) {
@@ -152,7 +152,34 @@ describe('ListArticles', () => {
           1. text -> ariticles linked to the content
           居有頃，倚柱彈其劍，歌曰：「長鋏歸來乎！食無魚！」左右以告。孟嘗君曰：「食之
           ，比門下之客。」居有頃，復彈其鋏，歌曰：「長鋏歸來乎！出無車！」
+        `,
+      })
+    ).toMatchSnapshot();
+  });
 
+  it("filters by moreLikeThis and given URL, find articles with content the said URL's content", async () => {
+    // moreLikeThis query
+    // 1, given text, find articles containing hyperlinks with the said text
+    // 2. given URL, find articles with content the said URL's content
+    expect(
+      await gql`
+        query($like: String) {
+          ListArticles(
+            filter: { moreLikeThis: { like: $like, minimumShouldMatch: "5%" } }
+          ) {
+            edges {
+              node {
+                id
+                hyperlinks {
+                  summary
+                  topImageUrl
+                }
+              }
+            }
+          }
+        }
+      `({
+        like: `
           2. URL -> article with given URL's content
           http://出師表.com
         `,
