35012db9b4d7fa8e66edafafb51fb255e88920dd
MrOrz <johnsonliang7@gmail.com>
2019-01-09 02:15:58 +0800

    Allow feedback count to be > 10

--- src/graphql/mutations/__fixtures__/CreateOrUpdateArticleReplyFeedback.js
+++ src/graphql/mutations/__fixtures__/CreateOrUpdateArticleReplyFeedback.js
@@ -1,26 +1,107 @@
 import { getArticleReplyFeedbackId } from '../CreateOrUpdateArticleReplyFeedback';
 
+function generateEntry(data) {
+  return {
+    [`/articlereplyfeedbacks/doc/${getArticleReplyFeedbackId(data)}`]: data,
+  };
+}
+
 export default {
   '/articles/doc/article1': {
     articleReplies: [
       {
         replyId: 'reply1',
-        positiveFeedbackCount: 1,
+        positiveFeedbackCount: 11,
         negativeFeedbackCount: 0,
       },
     ],
   },
-  [`/articlereplyfeedbacks/doc/${getArticleReplyFeedbackId({
-    articleId: 'article1',
-    replyId: 'reply1',
-    userId: 'testUser',
-    appId: 'testClient',
-  })}`]: {
+  ...generateEntry({
     score: 1,
     articleId: 'article1',
     replyId: 'reply1',
     appId: 'testClient',
     userId: 'testUser',
     createdAt: '2017-01-01T00:00:00.000Z',
-  },
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    replyId: 'reply1',
+    appId: 'testClient',
+    userId: 'testUser2',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    replyId: 'reply1',
+    appId: 'testClient',
+    userId: 'testUser3',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    replyId: 'reply1',
+    appId: 'testClient',
+    userId: 'testUser4',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    replyId: 'reply1',
+    appId: 'testClient',
+    userId: 'testUser5',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    replyId: 'reply1',
+    appId: 'testClient',
+    userId: 'testUser6',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    replyId: 'reply1',
+    appId: 'testClient',
+    userId: 'testUser7',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    replyId: 'reply1',
+    appId: 'testClient',
+    userId: 'testUser8',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    replyId: 'reply1',
+    appId: 'testClient',
+    userId: 'testUser9',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    replyId: 'reply1',
+    appId: 'testClient',
+    userId: 'testUser10',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    replyId: 'reply1',
+    appId: 'testClient',
+    userId: 'testUser11',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
 };
