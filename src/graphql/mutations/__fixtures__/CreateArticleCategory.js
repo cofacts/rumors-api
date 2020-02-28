011cab91282d5ed42ffff033baf1a6d547d23000
ggm <godgunman@gmail.com>
2020-02-28 15:36:42 +0800

    Add tests for CreateCategory and CreateArticleCategory

---
+++ src/graphql/mutations/__fixtures__/CreateArticleCategory.js
@@ -0,0 +1,11 @@
+export default {
+  '/articles/doc/createArticleCategory1': {
+    text: 'foofoo',
+    articleCategories: [],
+    references: [{ type: 'LINE' }],
+  },
+  '/categories/doc/createArticleCategory2': {
+    title: 'bar',
+    description: 'RUMOR',
+  },
+};
---
+++ src/graphql/mutations/__tests__/CreateCategory.js
@@ -0,0 +1,38 @@
+jest.mock('util/grpc');
+
+import gql from 'util/GraphQL';
+import client from 'util/client';
+import MockDate from 'mockdate';
+
+describe('CreateCategory', () => {
+  it('creates a category', async () => {
+    MockDate.set(1485593157011);
+    const { data, errors } = await gql`
+      mutation($title: String!, $description: String!) {
+        CreateCategory(title: $title, description: $description) {
+          id
+        }
+      }
+    `(
+      {
+        title: '保健秘訣、食品安全',
+        description:
+          '各種宣稱會抗癌、高血壓、糖尿病等等的偏方秘笈、十大恐怖美食、不要吃海帶、美粒果',
+      },
+      { userId: 'test', appId: 'test' }
+    );
+
+    const categoryId = data.CreateCategory.id;
+    const category = await client.get({
+      index: 'categories',
+      type: 'doc',
+      id: categoryId,
+    });
+
+    expect(errors).toBeUndefined();
+    expect(category._source).toMatchSnapshot('created category');
+
+    // Cleanup
+    await client.delete({ index: 'categories', type: 'doc', id: categoryId });
+  });
+});
