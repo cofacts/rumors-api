828a3c8b519aef9044e1a665ee8a0f19c363d630
ggm <godgunman@gmail.com>
2020-02-29 14:05:47 +0800

    Add tests for CreateOrUpdateArticleCategoryFeedback

---
+++ src/graphql/mutations/__fixtures__/CreateOrUpdateArticleCategoryFeedback.js
@@ -0,0 +1,109 @@
+import { getArticleCategoryFeedbackId } from '../CreateOrUpdateArticleCategoryFeedback';
+
+function generateEntry(data) {
+  return {
+    [`/articlecategoryfeedbacks/doc/${getArticleCategoryFeedbackId(
+      data
+    )}`]: data,
+  };
+}
+
+export default {
+  '/articles/doc/article1': {
+    articleCategories: [
+      {
+        categoryId: 'category1',
+        positiveFeedbackCount: 11,
+        negativeFeedbackCount: 0,
+      },
+    ],
+  },
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser2',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser3',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser4',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser5',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser6',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser7',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser8',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser9',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser10',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+  ...generateEntry({
+    score: 1,
+    articleId: 'article1',
+    categoryId: 'category1',
+    appId: 'testClient',
+    userId: 'testUser11',
+    createdAt: '2017-01-01T00:00:00.000Z',
+  }),
+};
---
+++ src/graphql/mutations/__tests__/CreateOrUpdateArticleCategoryFeedback.js
@@ -0,0 +1,137 @@
+import gql from 'util/GraphQL';
+import { loadFixtures, unloadFixtures, resetFrom } from 'util/fixtures';
+import client from 'util/client';
+import { getArticleCategoryFeedbackId } from '../CreateOrUpdateArticleCategoryFeedback';
+import MockDate from 'mockdate';
+import fixtures from '../__fixtures__/CreateOrUpdateArticleCategoryFeedback';
+
+describe('CreateOrUpdateArticleCategoryFeedback', () => {
+  beforeAll(() => loadFixtures(fixtures));
+
+  it('creates feedback on given article category', async () => {
+    MockDate.set(1485593157011);
+    const userId = 'test';
+    const appId = 'test';
+    const articleId = 'article1';
+    const categoryId = 'category1';
+    const comment = 'comment1';
+
+    const { data, errors } = await gql`
+      mutation($articleId: String!, $categoryId: String!, $comment: String!) {
+        CreateOrUpdateArticleCategoryFeedback(
+          articleId: $articleId
+          categoryId: $categoryId
+          vote: UPVOTE
+          comment: $comment
+        ) {
+          articleId
+          categoryId
+          feedbackCount
+          positiveFeedbackCount
+          negativeFeedbackCount
+          ownVote
+          feedbacks {
+            vote
+            comment
+          }
+        }
+      }
+    `(
+      {
+        articleId,
+        categoryId,
+        comment,
+      },
+      { userId, appId, comment }
+    );
+    MockDate.reset();
+
+    expect(errors).toBeUndefined();
+    expect(data).toMatchSnapshot();
+
+    const id = getArticleCategoryFeedbackId({
+      articleId,
+      categoryId,
+      userId,
+      appId,
+    });
+
+    const conn = await client.get({
+      index: 'articlecategoryfeedbacks',
+      type: 'doc',
+      id,
+    });
+    expect(conn._source).toMatchSnapshot();
+
+    const article = await client.get({
+      index: 'articles',
+      type: 'doc',
+      id: articleId,
+    });
+    expect(article._source.articleCategories).toMatchSnapshot();
+
+    // Cleanup
+    await client.delete({
+      index: 'articlecategoryfeedbacks',
+      type: 'doc',
+      id,
+    });
+    await resetFrom(fixtures, '/articles/doc/article1');
+  });
+
+  it('updates existing feedback', async () => {
+    MockDate.set(1485593157011);
+    const userId = 'testUser';
+    const appId = 'testClient';
+    const articleId = 'article1';
+    const categoryId = 'category1';
+
+    const { data, errors } = await gql`
+      mutation($articleId: String!, $categoryId: String!) {
+        CreateOrUpdateArticleCategoryFeedback(
+          articleId: $articleId
+          categoryId: $categoryId
+          vote: DOWNVOTE
+        ) {
+          articleId
+          categoryId
+          feedbackCount
+          positiveFeedbackCount
+          negativeFeedbackCount
+          ownVote
+          feedbacks {
+            vote
+            comment
+          }
+        }
+      }
+    `(
+      {
+        articleId,
+        categoryId,
+      },
+      { userId, appId }
+    );
+    MockDate.reset();
+
+    expect(errors).toBeUndefined();
+    expect(data).toMatchSnapshot();
+
+    const id = getArticleCategoryFeedbackId({
+      articleId,
+      categoryId,
+      userId,
+      appId,
+    });
+
+    expect(
+      (await client.get({ index: 'articlecategoryfeedbacks', type: 'doc', id }))
+        ._source
+    ).toMatchSnapshot();
+
+    // Cleanup
+    await resetFrom(fixtures, `/articlecategoryfeedbacks/doc/${id}`);
+  });
+
+  afterAll(() => unloadFixtures(fixtures));
+});
