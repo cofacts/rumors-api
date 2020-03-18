1241c0a971d6702697e1e6c50476d90205ce00e1
ggm <godgunman@gmail.com>
2020-03-18 20:02:24 +0800

    Add tests for ListCategories

---
+++ src/graphql/queries/__fixtures__/ListCategories.js
@@ -0,0 +1,21 @@
+export default {
+  '/categories/doc/c1': {
+    title: '性少數與愛滋病',
+    description: '對同性婚姻的恐懼、愛滋病的誤解與的防疫相關釋疑。',
+    createdAt: '2020-02-06T05:34:45.862Z',
+    updatedAt: '2020-02-06T05:34:45.862Z',
+  },
+  '/categories/doc/c2': {
+    title: '女權與性別刻板印象',
+    description: '對性別平等教育的恐慌與抹黑',
+    createdAt: '2020-02-06T05:34:46.862Z',
+    updatedAt: '2020-02-06T05:34:46.862Z',
+  },
+  '/categories/doc/c3': {
+    title: '保健秘訣、食品安全',
+    description:
+      '各種宣稱會抗癌、高血壓、糖尿病等等的偏方秘笈、十大恐怖美食、不要吃海帶、美粒果',
+    createdAt: '2020-02-06T05:34:43.862Z',
+    updatedAt: '2020-02-06T05:34:43.862Z',
+  },
+};
