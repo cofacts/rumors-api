47a609efddba27fed4c879e232afc8d7bbf94ea3
Johnson Liang <johnson.liang@appier.com>
2020-05-26 14:08:54 +0800

    [ArticleReplyFeedback] add article & reply & articleReply field

--- src/graphql/models/ArticleReplyFeedback.js
+++ src/graphql/models/ArticleReplyFeedback.js
@@ -2,6 +2,9 @@ import { GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql';
 import FeedbackVote from './FeedbackVote';
 
 import User, { userFieldResolver } from './User';
+import Article from './Article';
+import Reply from './Reply';
+import ArticleReply from './ArticleReply';
 
 export default new GraphQLObjectType({
   name: 'ArticleReplyFeedback',
@@ -27,6 +30,32 @@ export default new GraphQLObjectType({
       description:
         'One of 1, 0 and -1. Representing upvote, neutral and downvote, respectively',
       type: GraphQLInt,
+    },
+
+    article: {
+      type: Article,
+      description: "The scored article-reply's article",
+      resolve: ({ articleId }, args, { loaders }) =>
+        loaders.docLoader.load({ index: 'articles', id: articleId }),
+    },
+
+    reply: {
+      type: Reply,
+      description: "The scored article-reply's reply",
+      resolve: ({ replyId }, args, { loaders }) =>
+        loaders.docLoader.load({ index: 'replies', id: replyId }),
+    },
+
+    articleReply: {
+      type: ArticleReply,
+      description: 'The scored article-reply',
+      resolve: async ({ articleId, replyId }, args, { loaders }) => {
+        const { articleReplies } = await loaders.docLoader.load({
+          index: 'articles',
+          id: articleId,
+        });
+        return articleReplies.find(ar => ar.replyId === replyId);
+      },
     },
   }),
 });
