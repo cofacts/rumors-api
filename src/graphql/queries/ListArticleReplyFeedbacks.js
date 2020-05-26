84c53d3b5ef5760a31c45d78a30939a2bdcd3108
MrOrz <johnsonliang7@gmail.com>
2020-05-27 01:58:51 +0800

    [ListArticleReplyFeedback] stub for filter processing and test fixtures

--- src/graphql/queries/ListArticleReplyFeedbacks.js
+++ src/graphql/queries/ListArticleReplyFeedbacks.js
@@ -21,14 +21,20 @@ export default {
         appId: {
           type: GraphQLString,
         },
+        articleId: {
+          type: GraphQLString,
+        },
+        replyId: {
+          type: GraphQLString,
+        },
         moreLikeThis: {
           type: moreLikeThisInput,
-          description: 'Search for comment using more_like_this query',
+          description: 'Search for comment field using more_like_this query',
         },
-        score: {
+        vote: {
           type: new GraphQLList(FeedbackVote),
           description:
-            'When specified, list only article reply feedbacks with specified score',
+            'When specified, list only article reply feedbacks with specified vote',
         },
         createdAt: {
           type: timeRangeInput,
@@ -46,14 +52,33 @@ export default {
       type: createSortType('ListArticleReplyFeedbackOrderBy', [
         'createdAt',
         'updatedAt',
-        'score',
+        'vote',
+        {
+          name: '_score',
+          description: 'Full text relevance for comment field queries',
+        },
       ]),
     },
     ...pagingArgs,
   },
-  async resolve(rootValue, { orderBy = [], ...otherParams }) {
+  async resolve(rootValue, { orderBy = [], filter = {}, ...otherParams }) {
     const body = {
-      sort: getSortArgs(orderBy),
+      sort: getSortArgs(orderBy, {
+        vote: o => ({ score: { order: o } }),
+      }),
+      track_scores: true, // for _score sorting
+    };
+
+    const shouldQueries = []; // Affects scores
+    const filterQueries = []; // Not affects scores
+
+    body.query = {
+      bool: {
+        should:
+          shouldQueries.length === 0 ? [{ match_all: {} }] : shouldQueries,
+        filter: filterQueries,
+        minimum_should_match: 1, // At least 1 "should" query should present
+      },
     };
 
     // should return search context for resolveEdges & resolvePageInfo
