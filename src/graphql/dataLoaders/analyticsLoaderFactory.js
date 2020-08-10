f89ea47f8794c8792cd0ca236912438a484403c1
Zoe <1817638+ztsai@users.noreply.github.com>
2020-08-10 16:48:16 +0800

    add cachedKey&size; change start, end date pair to dateRange

--- src/graphql/dataLoaders/analyticsLoaderFactory.js
+++ src/graphql/dataLoaders/analyticsLoaderFactory.js
@@ -1,54 +1,57 @@
 import DataLoader from 'dataloader';
 import client from 'util/client';
-import { assertDateRange } from 'util/date';
+import { getRangeFieldParamFromArithmeticExpression } from 'graphql/util';
 
 const defaultDuration = 30 * 24 * 60 * 60 * 1000;
 
 export default () =>
-  new DataLoader(async statsQueries => {
-    const body = [];
-    const now = new Date();
-    const defaultEndDate = now.toISOString().substr(0, 10);
-    const defaultStartDate = new Date(now - defaultDuration)
-      .toISOString()
-      .substr(0, 10);
-
-    statsQueries.forEach(({ docId, docType, startDate, endDate }) => {
-      if (!docId) throw new Error('docId is required');
-      if (!docType) throw new Error('docType is required');
-
-      if (!startDate && !endDate) {
-        startDate = defaultStartDate;
-        endDate = defaultEndDate;
-      } else {
-        assertDateRange(startDate, endDate);
-      }
-
-      body.push({ index: 'analytics', type: 'doc' });
-      body.push({
-        query: {
-          bool: {
-            must: [
-              { match: { type: docType } },
-              { match: { docId: docId } },
-              {
-                range: {
-                  date: {
-                    gte: startDate,
-                    lte: endDate,
+  new DataLoader(
+    async statsQueries => {
+      const body = [];
+      const now = new Date();
+      const defaultEndDate = now.toISOString().substr(0, 10);
+      const defaultStartDate = new Date(now - defaultDuration)
+        .toISOString()
+        .substr(0, 10);
+      const defaultDateRange = {
+        gte: defaultStartDate,
+        lte: defaultEndDate,
+      };
+      statsQueries.forEach(
+        ({ docId, docType, dateRange = defaultDateRange }) => {
+          if (!docId) throw new Error('docId is required');
+          if (!docType) throw new Error('docType is required');
+          body.push({ index: 'analytics', type: 'doc' });
+          body.push({
+            query: {
+              bool: {
+                must: [
+                  { match: { type: docType } },
+                  { match: { docId: docId } },
+                  {
+                    range: {
+                      date: getRangeFieldParamFromArithmeticExpression(
+                        dateRange
+                      ),
+                    },
                   },
-                },
+                ],
               },
-            ],
-          },
-        },
-        sort: [{ date: 'asc' }],
-      });
-    });
+            },
+            sort: [{ date: 'asc' }],
+            size: 90,
+          });
+        }
+      );
 
-    return (await client.msearch({
-      body,
-    })).body.responses.map(({ hits: { hits: analytics } }) =>
-      analytics.map(row => ({ date: row._source.date, ...row._source.stats }))
-    );
-  });
+      return (await client.msearch({
+        body,
+      })).body.responses.map(({ hits: { hits: analytics } }) =>
+        analytics.map(row => ({ date: row._source.date, ...row._source.stats }))
+      );
+    },
+    {
+      cacheKeyFn: ({ docId, docType, dateRange }) =>
+        `${docType}/${docId}/${JSON.stringify(dateRange)}`,
+    }
+  );
