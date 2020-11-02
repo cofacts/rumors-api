6abb340ad1470d2d1ff43ba0564084bc7551a596
Zoe <1817638+ztsai@users.noreply.github.com>
2020-11-03 00:18:50 +0800

    add post test and test sequencer

--- test/postTest.js
+++ test/postTest.js
@@ -9,10 +9,16 @@ const checkDocs = async () => {
   if (total > 0) {
     console.log('\x1b[33m');
     console.log('WARNING: test db is not cleaned up properly.');
-    console.log(JSON.stringify(hits.map(d => `/${d._index}/${d._type}/${d._id}`), null, 2));
+    const docs = hits.map(d => `/${d._index}/${d._type}/${d._id}`)
+    console.log(JSON.stringify(docs, null, 2));
     console.log('\x1b[0m');
+
+    for (const d of hits) {
+      await client.delete({index: d._index, type: d._type, id: d._id})
+    }
+    throw new Error()
   }    
 }
 
-checkDocs();
+checkDocs()
 
