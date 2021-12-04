af83c9977eb9819c1785d0bfdfb7ba355996cf6d
MrOrz <johnsonliang7@gmail.com>
2021-12-04 15:51:16 +0800

    [importFlowAnnotation] add test for processEntry

---
+++ src/scripts/migrations/__fixtures__/importFlowAnnotation.js
@@ -0,0 +1,75 @@
+import { getArticleCategoryFeedbackId } from 'graphql/mutations/CreateOrUpdateArticleCategoryFeedback';
+
+export default {
+  '/articles/doc/a1': {
+    text: 'Article text',
+    normalArticleCategoryCount: 2,
+    articleCategories: [
+      // Already existed, not commented
+      {
+        categoryId: 'kj287XEBrIRcahlYvQoS',
+        positiveFeedbackCount: 0,
+        negativeFeedbackCount: 0,
+        status: 'NORMAL',
+      },
+
+      // Already existed and commented previously
+      {
+        categoryId: 'kz3c7XEBrIRcahlYxAp6',
+        positiveFeedbackCount: 1,
+        negativeFeedbackCount: 1,
+        status: 'NORMAL',
+      },
+
+      // Previously tagged and then deleted
+      {
+        categoryId: 'lD3h7XEBrIRcahlYeQqS',
+        positiveFeedbackCount: 0,
+        negativeFeedbackCount: 0,
+        status: 'DELETED',
+      },
+    ],
+  },
+  [`/articlecategoryfeedbacks/doc/${getArticleCategoryFeedbackId({
+    articleId: 'a1',
+    categoryId: 'kz3c7XEBrIRcahlYxAp6',
+    userId: 'non-reviewer',
+    appId: 'WEBSITE',
+  })}`]: {
+    articleId: 'a1',
+    categoryId: 'kz3c7XEBrIRcahlYxAp6',
+    appId: 'WEBSITE',
+    userId: 'non-reviewer',
+    score: 1,
+    status: 'NORMAL',
+  },
+  [`/articlecategoryfeedbacks/doc/${getArticleCategoryFeedbackId({
+    articleId: 'a1',
+    categoryId: 'kz3c7XEBrIRcahlYxAp6',
+    userId: 'category-reviewer',
+    appId: 'RUMORS_AI',
+  })}`]: {
+    articleId: 'a1',
+    categoryId: 'kz3c7XEBrIRcahlYxAp6',
+    appId: 'RUMORS_AI',
+    userId: 'category-reviewer',
+    score: -1,
+    status: 'NORMAL',
+  },
+  '/categories/doc/kj287XEBrIRcahlYvQoS': {
+    title: 'Category 1',
+    description: 'Description for category 1',
+  },
+  '/categories/doc/kz3c7XEBrIRcahlYxAp6': {
+    title: 'Category 2',
+    description: 'Description for category 2',
+  },
+  '/categories/doc/lD3h7XEBrIRcahlYeQqS': {
+    title: 'Category 3',
+    description: 'Description for category 3',
+  },
+  '/categories/doc/lT3h7XEBrIRcahlYugqq': {
+    title: 'Category 4',
+    description: 'Description for category 4',
+  },
+};
---
+++ src/scripts/migrations/__tests__/importFlowAnnotation.js
@@ -0,0 +1,119 @@
+import MockDate from 'mockdate';
+import { processEntry } from '../importFlowAnnotation';
+import { loadFixtures, unloadFixtures } from 'util/fixtures';
+import client from 'util/client';
+import fixtures from '../__fixtures__/importFlowAnnotation';
+
+beforeEach(() => loadFixtures(fixtures));
+afterEach(() => unloadFixtures(fixtures));
+
+const FIXED_DATE = 612921600000;
+
+it('adds article category and feedbacks as expected', async () => {
+  MockDate.set(FIXED_DATE);
+  // Add 4 categories to a1
+  await processEntry({
+    id: 'a1',
+    tags: [0, 1, 2, 3],
+  });
+  MockDate.reset();
+
+  const {
+    body: { _source: articleDoc },
+  } = await client.get({
+    index: 'articles',
+    type: 'doc',
+    id: 'a1',
+  });
+
+  expect(articleDoc.normalArticleCategoryCount).toBe(4);
+
+  // Expect all 4 articleCategories and feedback has been updated
+  //
+  expect(articleDoc.articleCategories).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "categoryId": "kj287XEBrIRcahlYvQoS",
+        "negativeFeedbackCount": 0,
+        "positiveFeedbackCount": 1,
+        "status": "NORMAL",
+      },
+      Object {
+        "categoryId": "kz3c7XEBrIRcahlYxAp6",
+        "negativeFeedbackCount": 0,
+        "positiveFeedbackCount": 2,
+        "status": "NORMAL",
+      },
+      Object {
+        "appId": "RUMORS_AI",
+        "categoryId": "lD3h7XEBrIRcahlYeQqS",
+        "negativeFeedbackCount": 0,
+        "positiveFeedbackCount": 1,
+        "status": "NORMAL",
+        "updatedAt": "1989-06-04T00:00:00.000Z",
+        "userId": "flow-annotator",
+      },
+      Object {
+        "appId": "RUMORS_AI",
+        "categoryId": "lT3h7XEBrIRcahlYugqq",
+        "createdAt": "1989-06-04T00:00:00.000Z",
+        "negativeFeedbackCount": 0,
+        "positiveFeedbackCount": 1,
+        "status": "NORMAL",
+        "updatedAt": "1989-06-04T00:00:00.000Z",
+        "userId": "flow-annotator",
+      },
+    ]
+  `);
+
+  const {
+    body: {
+      hits: { total, hits: articleCategoryFeedbacks },
+    },
+  } = await client.search({
+    index: 'articlecategoryfeedbacks',
+    body: {
+      query: {
+        term: {
+          appId: 'RUMORS_AI',
+        },
+      },
+    },
+  });
+
+  expect(total).toBe(4);
+
+  const articleCategoryFeedback = articleCategoryFeedbacks.find(
+    ({ _source }) => _source.categoryId === 'kz3c7XEBrIRcahlYxAp6'
+  );
+  expect(articleCategoryFeedback).toMatchInlineSnapshot(`
+    Object {
+      "_id": "a1__kz3c7XEBrIRcahlYxAp6__category-reviewer__RUMORS_AI",
+      "_index": "articlecategoryfeedbacks_v1_1_1",
+      "_score": 0.24116206,
+      "_source": Object {
+        "appId": "RUMORS_AI",
+        "articleId": "a1",
+        "categoryId": "kz3c7XEBrIRcahlYxAp6",
+        "comment": "若水標記之分類",
+        "score": 1,
+        "status": "NORMAL",
+        "updatedAt": "1989-06-04T00:00:00.000Z",
+        "userId": "category-reviewer",
+      },
+      "_type": "doc",
+    }
+  `);
+
+  // Cleanup
+  await client.deleteByQuery({
+    index: 'articlecategoryfeedbacks',
+    body: {
+      query: {
+        term: {
+          appId: 'RUMORS_AI',
+        },
+      },
+    },
+  });
+});
