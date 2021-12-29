60d435a9e424df27d25820cf8ca32b873310ef19
Nonumpa <acerxp511@msn.com>
2021-12-29 20:32:11 +0800

    [CreateMediaArticle] test graphql CreateMediaArticle

---
+++ src/graphql/mutations/__fixtures__/CreateMediaArticle.js
@@ -0,0 +1,11 @@
+export default {
+  [`/articles/doc/image1`]: {
+    text: '',
+    attachment: {
+      hash: 'ffff8000',
+      mediaUrl: 'http://foo/image.jpeg',
+    },
+    replyRequestCount: 1,
+    references: [{ type: 'LINE' }],
+  },
+};
