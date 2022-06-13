fc40aa056646c92f9422f60e296579d240231b34
MrOrz <johnsonliang7@gmail.com>
2022-06-13 15:11:30 +0800

    [ListArticles] Fix typo

--- src/graphql/queries/ListArticles.js
+++ src/graphql/queries/ListArticles.js
@@ -129,7 +129,7 @@ export default {
               },
 
               replyTypes: {
-                tupe: new GraphQLList(ReplyTypeEnum),
+                type: new GraphQLList(ReplyTypeEnum),
               },
             },
           }),
