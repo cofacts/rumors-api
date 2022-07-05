feda72cfa6742170812c7e41db3f66d27d41cab0
Johnson Liang <johnsonliang7@gmail.com>
2022-07-05 13:51:09 +0800

    Apply variant settings to images

--- src/util/mediaManager.js
+++ src/util/mediaManager.js
@@ -1,9 +1,35 @@
-import { MediaManager } from '@cofacts/media-manager';
+import sharp from 'sharp';
+import { MediaManager, variants } from '@cofacts/media-manager';
 
 const mediaManager = new MediaManager({
   bucketName: process.env.GCS_BUCKET_NAME,
   credentialsJSON: process.env.GCS_CREDENTIALS,
   prefix: process.env.GCS_MEDIA_FOLDER,
+  getVariantSettings(options) {
+    const { type, contentType } = options;
+    switch (type) {
+      case 'image':
+        return [
+          variants.original(contentType),
+          {
+            name: 'jpg240h',
+            contentType: 'image/jpeg',
+            transform: sharp()
+              .resize({ height: 240, withoutEnlargement: true })
+              .jpeg({ quality: 60 }),
+          },
+          {
+            name: 'webp600w',
+            contentType: 'image/webp',
+            transform: sharp()
+              .resize({ width: 600, withoutEnlargement: true })
+              .webp({ quality: 60 }),
+          },
+        ];
+      default:
+        return variants.defaultGetVariantSettings(options);
+    }
+  },
 });
 
 export default mediaManager;
