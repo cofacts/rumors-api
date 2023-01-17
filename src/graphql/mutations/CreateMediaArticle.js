b60bd4ebe10e39fe7626e04a89cf07745ca358ae
Johnson Liang <johnsonliang7@gmail.com>
2023-01-17 13:41:49 +0800

    fix(CreateMediaArticle) fix mediaEntry type -- should be mediaEntry rather than a promise

--- src/graphql/mutations/CreateMediaArticle.js
+++ src/graphql/mutations/CreateMediaArticle.js
@@ -31,9 +31,9 @@ const VALID_ARTICLE_TYPE_TO_MEDIA_TYPE = {
  * @param {ArticleTypeEnum} param.articleType
  * @returns {Promise<MediaEntry>}
  */
-export function uploadMedia({ mediaUrl, articleType }) {
+export async function uploadMedia({ mediaUrl, articleType }) {
   const mappedMediaType = VALID_ARTICLE_TYPE_TO_MEDIA_TYPE[articleType];
-  const mediaEntry = mediaManager.insert({
+  const mediaEntry = await mediaManager.insert({
     url: mediaUrl,
     getVariantSettings(options) {
       const { type, contentType } = options;
