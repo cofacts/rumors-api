140b21df9a63b01548d837b028352fbd82c54612
MrOrz <johnsonliang7@gmail.com>
2023-04-01 21:13:36 +0800

    [CreateAIReply] more detailed happy case and a failing loading case

--- src/graphql/mutations/__fixtures__/CreateAIReply.js
+++ src/graphql/mutations/__fixtures__/CreateAIReply.js
@@ -6,6 +6,9 @@ export default {
   '/articles/doc/ai-replied-article': {
     text: 'foo',
   },
+  '/articles/doc/some-article': {
+    text: 'Some article',
+  },
   '/airesponses/doc/ai-reply-old': {
     docId: 'ai-replied-article',
     type: 'AI_REPLY',
@@ -17,5 +20,17 @@ export default {
     type: 'AI_REPLY',
     status: 'SUCCESS',
     createdAt: '2020-01-02T00:00:00.000Z',
+  },
+  '/airesponses/doc/expired-loading': {
+    docId: 'ai-replied-article',
+    type: 'AI_REPLY',
+    status: 'LOADING',
+    createdAt: '2020-01-04T00:00:00.000Z', // Should have expired by the time the tests are run.
+  },
+  '/airesponses/doc/loading': {
+    docId: 'some-article',
+    type: 'AI_REPLY',
+    status: 'LOADING',
+    createdAt: '2020-01-01T00:00:00.000Z', // Will be filled during test setup
   },
 };
--- src/graphql/mutations/__tests__/CreateAIReply.js
+++ src/graphql/mutations/__tests__/CreateAIReply.js
@@ -1,11 +1,12 @@
 jest.mock('util/openai');
-import openai from 'util/openai';
 import MockDate from 'mockdate';
 
-import gql from 'util/GraphQL';
-// import client from 'util/client';
+import openai from 'util/openai';
+import delayForMs from 'util/delayForMs';
 
-import { loadFixtures, unloadFixtures } from 'util/fixtures';
+import gql from 'util/GraphQL';
+
+import { loadFixtures, unloadFixtures, resetFrom } from 'util/fixtures';
 import fixtures from '../__fixtures__/CreateAIReply';
 import client from 'util/client';
 
@@ -61,42 +62,20 @@ describe('CreateAIReply', () => {
     `);
   });
 
-  // it('waits for existing loading AI reply')
-
   it('returns new AI response', async () => {
     // Mocked ChatGPT success response
     //
+    let resolveAPI;
     const mockFn = openai.createChatCompletion.mockImplementationOnce(
-      async () => ({
-        data: {
-          id: 'chatcmpl-some-id',
-          object: 'chat.completion',
-          created: 1679847676,
-          model: 'gpt-3.5-turbo-0301',
-          usage: {
-            prompt_tokens: 343,
-            completion_tokens: 64,
-            total_tokens: 407,
-          },
-          choices: [
-            {
-              message: {
-                role: 'assistant',
-                content:
-                  '閱聽人應該確保登記網站的正確性和安全性，並記得定期更改密碼和密鑰，以保護自己的資訊安全。',
-              },
-              finish_reason: 'stop',
-              index: 0,
-            },
-          ],
-        },
-        status: 200,
-        statusText: 'OK',
-      })
+      () =>
+        new Promise(resolve => {
+          resolveAPI = resolve;
+        })
     );
 
     MockDate.set(1602288000000);
-    const { data, errors } = await gql`
+
+    const resp = gql`
       mutation($articleId: String!) {
         CreateAIReply(articleId: $articleId) {
           id
@@ -116,6 +95,77 @@ describe('CreateAIReply', () => {
       },
       { user: { id: 'test', appId: 'test' } }
     );
+
+    await delayForMs(2000); // Wait a bit until airesponse is indexed
+
+    const {
+      body: {
+        hits: { hits: loadingAIReplies },
+      },
+    } = await client.search({
+      index: 'airesponses',
+      type: 'doc',
+      body: {
+        query: {
+          bool: {
+            must: [
+              { term: { type: 'AI_REPLY' } },
+              { term: { docId: 'reported-article' } },
+            ],
+          },
+        },
+      },
+    });
+
+    // Expect we get a loading reply before API resolves.
+    //
+    expect(loadingAIReplies).toHaveLength(1);
+    const {
+      _id: dontcare, // eslint-disable-line no-unused-vars
+      ...loadingAIReply
+    } = loadingAIReplies[0]._source;
+    expect(loadingAIReply).toMatchInlineSnapshot(`
+      Object {
+        "appId": "test",
+        "createdAt": "2020-10-10T00:00:00.000Z",
+        "docId": "reported-article",
+        "request": "{\\"model\\":\\"gpt-3.5-turbo\\",\\"messages\\":[{\\"role\\":\\"system\\",\\"content\\":\\"今天是2020年10月10日。你是協助讀者進行媒體識讀的小幫手。你說話時總是使用台灣繁體中文。有讀者傳了一則網路訊息給你。\\"},{\\"role\\":\\"user\\",\\"content\\":\\"我優秀的斐陶斐大姐是中央銀行退休，她剛看了一下，上網登記除要身份証號碼，還要健保卡號，健保卡號很少會要求提供，被洩漏機會相對少，但這次登記要一次完整的登入雙證件的號碼有點讓人擔憂，連同銀行帳號一併洩漏後果可怕！ \\"},{\\"role\\":\\"user\\",\\"content\\":\\"請問作為閱聽人，我應該注意這則訊息的哪些地方呢？\\\\n請節錄訊息中需要特別留意的地方，說明為何閱聽人需要注意它，謝謝。\\"}]}",
+        "status": "LOADING",
+        "type": "AI_REPLY",
+        "userId": "test",
+      }
+    `);
+
+    // Simulates API resolves
+    //
+    resolveAPI({
+      data: {
+        id: 'chatcmpl-some-id',
+        object: 'chat.completion',
+        created: 1679847676,
+        model: 'gpt-3.5-turbo-0301',
+        usage: {
+          prompt_tokens: 343,
+          completion_tokens: 64,
+          total_tokens: 407,
+        },
+        choices: [
+          {
+            message: {
+              role: 'assistant',
+              content:
+                '閱聽人應該確保登記網站的正確性和安全性，並記得定期更改密碼和密鑰，以保護自己的資訊安全。',
+            },
+            finish_reason: 'stop',
+            index: 0,
+          },
+        ],
+      },
+      status: 200,
+      statusText: 'OK',
+    });
+
+    const { data, errors } = await resp;
     MockDate.reset();
 
     expect(errors).toBeUndefined();
@@ -145,6 +195,71 @@ describe('CreateAIReply', () => {
       id,
     });
   });
+
+  it(
+    'waits for existing loading AI reply',
+    async () => {
+      // Prepare a recently loading AI reply.
+      // Date cannot be mocked on NodeJS, because CreateAIReply calls Elasticsearch to calculate date.
+      //
+      await client.update({
+        index: 'airesponses',
+        type: 'doc',
+        id: 'loading',
+        body: {
+          doc: {
+            createdAt: new Date(),
+          },
+        },
+        refresh: 'true',
+      });
+
+      let isAIReplyPromiseResolved = false;
+      const createAIReplyPromise = await gql`
+        mutation($articleId: String!) {
+          CreateAIReply(articleId: $articleId) {
+            id
+            status
+          }
+        }
+      `(
+        {
+          articleId: fixtures['/airesponses/doc/loading'].docId,
+        },
+        { user: { id: 'test', appId: 'test' } }
+      ).then(ret => {
+        console.log(JSON.stringify({ ret }));
+        isAIReplyPromiseResolved = true;
+        return ret;
+      });
+
+      // Wait for some time for the loop in CreateAIReply to repeat itself.
+      delayForMs(2000);
+      expect(isAIReplyPromiseResolved).toBe(false);
+
+      // Simulate that the doc is resolved by another mutation
+      //
+      await client.update({
+        index: 'airesponses',
+        type: 'doc',
+        id: 'loading',
+        body: {
+          doc: {
+            status: 'SUCCESS',
+            updatedAt: new Date(),
+          },
+        },
+        refresh: 'true',
+      });
+
+      // Expect the promise to resolve to the already loaded AI reply
+      expect(await createAIReplyPromise).toMatchInlineSnapshot();
+
+      // Cleanup
+      await resetFrom(fixtures, '/airesponses/doc/loading');
+    },
+    10000
+  );
 
   it('returns API error', async () => {
     // Mocked ChatGPT failed response, simulate API key error
