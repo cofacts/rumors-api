4ae216dae396743e909fcf74c8595207a0bf1b61
MrOrz <johnsonliang7@gmail.com>
2023-06-26 01:43:17 +0800

    feat(fetStatsFromGA): fix BQ field schema - `event_date` is somehow interpreted as numbers, need to specify schema to make it a string - Temporarilly cancels MockDate because it mess up with Google auth

--- src/scripts/__tests__/fetchStatsFromGA.js
+++ src/scripts/__tests__/fetchStatsFromGA.js
@@ -3,28 +3,62 @@ import path from 'path';
 import MockDate from 'mockdate';
 
 // import { loadFixtures, unloadFixtures } from 'util/fixtures';
-// import client from 'util/client';
+import client from 'util/client';
+import delayForMs from 'util/delayForMs';
 
 // import fixtures from '../__fixtures__/fetchStatsFromGA';
 import { fetchStatsFromGA } from '../fetchStatsFromGA';
 
-const bigquery = new BigQuery();
-
 // The test will run on a fake dataset specified by TEST_DATASET env.
 // Please ensure that application default credential has the [permission to modify the dataset](https://cloud.google.com/bigquery/docs/batch-loading-data?hl=en#permissions-load-data-into-bigquery).
+
+const bigquery = new BigQuery();
+
+const COMMON_FETCHSTAT_PARAMS = {
+  timezone: 'Asia/Taipei',
+  lineBotEventDataset: process.env.TEST_DATASET,
+  ga4Dataset: process.env.TEST_DATASET,
+  webStreamId: 'website',
+  liffStreamId: 'liff',
+};
+
+const GA4_SCHEMA = {
+  fields: [
+    { name: 'event_name', type: 'STRING' },
+    { name: 'event_date', type: 'STRING' },
+    { name: 'event_timestamp', type: 'INTEGER' },
+    { name: 'user_pseudo_id', type: 'STRING' },
+    { name: 'stream_id', type: 'STRING' },
+    {
+      name: 'items',
+      type: 'RECORD',
+      mode: 'REPEATED',
+      fields: [
+        { name: 'item_id', type: 'STRING' },
+        { name: 'item_category', type: 'STRING' },
+      ],
+    },
+    {
+      name: 'collected_traffic_source',
+      type: 'RECORD',
+      fields: [{ name: 'manual_source', type: 'STRING' }],
+    },
+  ],
+};
 
 /**
  * @param {string} table
  * @param {string} jsonlFileName
  * @returns {Promise<JobMetadataResponse>}
  */
-function loadBqTable(table, jsonlFileName) {
+function loadBqTable(table, jsonlFileName, schema) {
   return bigquery
     .dataset(process.env.TEST_DATASET)
     .table(table)
     .load(path.join(__dirname, '../__fixtures__/', jsonlFileName), {
       sourceFormat: 'NEWLINE_DELIMITED_JSON',
-      autodetect: true,
+      autodetect: !schema,
+      schema,
       writeDisposition: 'WRITE_TRUNCATE',
     });
 }
@@ -37,24 +71,42 @@ if (process.env.TEST_DATASET) {
       // GA4 intraday table (Today = 20230604)
       loadBqTable(
         'events_intraday_20230604',
-        'fetchStatsFromGA-ga4-events_intraday.jsonl'
+        'fetchStatsFromGA-ga4-events_intraday.jsonl',
+        GA4_SCHEMA
       ),
 
       // GA4 daily event tables
       loadBqTable(
         'events_20230601',
-        'fetchStatsFromGA-ga4-events_20230601.jsonl'
+        'fetchStatsFromGA-ga4-events_20230601.jsonl',
+        GA4_SCHEMA
       ),
       loadBqTable(
         'events_20230602',
-        'fetchStatsFromGA-ga4-events_20230602.jsonl'
+        'fetchStatsFromGA-ga4-events_20230602.jsonl',
+        GA4_SCHEMA
       ),
 
       // LINE event table
       loadBqTable('events', 'fetchStatsFromGA-line-events.jsonl'),
+
+      // Cleanup analytics index
+      client.deleteByQuery({
+        index: 'analytics',
+        body: {
+          query: {
+            match_all: {},
+          },
+        },
+        refresh: 'true',
+      }),
     ]);
 
-    MockDate.set(1685877545000); // 20230604
+    // Wait for BQ & ES data to be indexed
+    //
+    await delayForMs(5000);
+
+    // MockDate.set(1685877545000); // 20230604
   }, 30000);
 
   afterAll(() => {
@@ -65,9 +117,28 @@ if (process.env.TEST_DATASET) {
     await expect(fetchStatsFromGA({ startDate: '20230601' })).rejects.toThrow(
       'Please provide both startDate and endDate'
     );
+    await expect(
+      fetchStatsFromGA({ startDate: 'haha', endDate: 'haha' })
+    ).rejects.toThrow('startDate must be in YYYYMMDD format');
+    await expect(
+      fetchStatsFromGA({ startDate: '20240601', endDate: 'haha' })
+    ).rejects.toThrow('endDate must be in YYYYMMDD format');
+    await expect(
+      fetchStatsFromGA({ startDate: '20770801', endDate: '20770802' })
+    ).rejects.toThrow('startDate must be earlier than 20230604');
+    await expect(
+      fetchStatsFromGA({ startDate: '20230601', endDate: '20770801' })
+    ).rejects.toThrow('endDate must be earlier than 20230604');
   });
 
-  // it('works on startDate & endDate', async () => {});
+  it.only('works on startDate & endDate', async () => {
+    // Fetch GA stats from BQ and write to analytics
+    await fetchStatsFromGA({
+      startDate: '20230601',
+      endDate: '20230602',
+      ...COMMON_FETCHSTAT_PARAMS,
+    });
+  });
 
   // it('works without startDate and endDate', async () => {});
 }
