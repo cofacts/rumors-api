1bd228686d95e9d59dd8be06ff73f91a1cf61b31
Nonumpa <acerxp511@msn.com>
2024-03-18 12:00:33 +0800

    feat(ListArticles): ListArticles can filter by contributors.userId

--- src/graphql/models/Contributor.js
+++ src/graphql/models/Contributor.js
@@ -1,8 +1,14 @@
 import { GraphQLObjectType, GraphQLString, GraphQLNonNull } from 'graphql';
+import User, { userFieldResolver } from './User';
 
 export default new GraphQLObjectType({
   name: 'Contributor',
   fields: () => ({
+    user: {
+      type: User,
+      description: 'The user who contributed to this article.',
+      resolve: userFieldResolver,
+    },
     userId: { type: GraphQLNonNull(GraphQLString) },
     appId: { type: GraphQLNonNull(GraphQLString) },
     updatedAt: { type: GraphQLString },
