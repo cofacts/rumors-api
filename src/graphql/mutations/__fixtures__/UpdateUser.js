1456502bbb5ce51e785dc901402eb1f997f61f5d
MrOrz <johnsonliang7@gmail.com>
2024-10-28 01:12:06 +0800

    fix(mutations): UpdateUser wrong fixture field

--- src/graphql/mutations/__fixtures__/UpdateUser.js
+++ src/graphql/mutations/__fixtures__/UpdateUser.js
@@ -1,29 +1,19 @@
 export default {
   '/users/doc/error': {
-    users: [
-      {
-        name: 'Bill',
-        updatedAt: 0,
-      },
-    ],
+    name: 'Bill',
+    updatedAt: 0,
   },
   '/users/doc/normal': {
-    users: [
-      {
-        name: 'Bill',
-        updatedAt: 0,
-      },
-    ],
+    name: 'Bill',
+    updatedAt: 0,
   },
   '/users/doc/testUser1': {
-    id: 'testUser1',
     name: 'test user 1',
     facebookId: 'fbid123',
     githubId: 'githubId123',
     email: 'user1@example.com',
   },
   '/users/doc/testUser2': {
-    id: 'testUser2',
     name: 'test user 2',
     githubId: 'githubId456',
     email: 'user2@example.com',
