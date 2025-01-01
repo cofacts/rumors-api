068680ff93d98092264626001f049c54526ca99f
MrOrz <johnsonliang7@gmail.com>
2025-01-02 00:16:30 +0800

    feat: setup langfuse

--- src/util/openai.js
+++ src/util/openai.js
@@ -1,9 +1,18 @@
 import { Configuration, OpenAIApi } from 'openai';
+import { Langfuse } from 'langfuse';
 
-const openai = new OpenAIApi(
-  new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  })
+const langfuse = new Langfuse({
+  publicKey: process.env.LANGFUSE_PUBLIC_KEY,
+  secretKey: process.env.LANGFUSE_SECRET_KEY,
+  baseUrl: process.env.LANGFUSE_HOST,
+});
+
+const openai = langfuse.observeOpenAI(
+  new OpenAIApi(
+    new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    })
+  )
 );
 
 export default openai;
