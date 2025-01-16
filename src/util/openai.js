9ce037769414603116a5d59b7e2c43a23fa24f12
MrOrz <johnsonliang7@gmail.com>
2025-01-17 02:08:13 +0800

    refactor: Improve OpenAI client initialization with cleaner arrow function syntax

--- src/util/openai.js
+++ src/util/openai.js
@@ -1,14 +1,15 @@
 import OpenAI from 'openai';
 import { observeOpenAI } from 'langfuse';
 
-const getOpenAI = (langfuseConfig = {}) => observeOpenAI(
-  new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-  }),
-  {
-    tags: [process.env.ROLLBAR_ENV],
-    ...langfuseConfig,
-  }
-);
+const getOpenAI = (langfuseConfig = {}) =>
+  observeOpenAI(
+    new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    }),
+    {
+      tags: [process.env.ROLLBAR_ENV],
+      ...langfuseConfig,
+    }
+  );
 
 export default getOpenAI;
