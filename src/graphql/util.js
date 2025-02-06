af44e548191ad0d99f316a5319cb4117fed0eeb4
Johnson Liang <johnsonliang7@gmail.com>
2025-02-06 14:22:11 +0800

    fix(util): seems that gemini model version cannot be omitted on vertex-ai Ref: https://cloud.google.com/vertex-ai/generative-ai/docs/gemini-v2

--- src/graphql/util.js
+++ src/graphql/util.js
@@ -856,7 +856,7 @@ function extractTextFromFullTextAnnotation(fullTextAnnotation) {
     .join('');
 }
 
-const TRANSCRIPT_MODELS = ['gemini-2.0-flash', 'gemini-1.5-pro-002'];
+const TRANSCRIPT_MODELS = ['gemini-2.0-flash-001', 'gemini-1.5-pro-002'];
 
 /**
  * @param {object} queryInfo - contains type and media entry ID of contents after fileUrl
