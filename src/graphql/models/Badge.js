91d5f32c6d5e53faac2e64746ff81f850c833356
EJ Huang <jhk482001@gmail.com>
2025-02-10 15:22:43 +0800

    update prettier

--- src/graphql/models/Badge.js
+++ src/graphql/models/Badge.js
@@ -1,4 +1,9 @@
-import { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLList } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLList
+} from 'graphql';
 
 export default new GraphQLObjectType({
   name: 'Badge',
@@ -9,7 +14,11 @@ export default new GraphQLObjectType({
     link: { type: new GraphQLNonNull(GraphQLString) },
     icon: { type: new GraphQLNonNull(GraphQLString) },
     borderImage: { type: new GraphQLNonNull(GraphQLString) },
-    issuers: { type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(GraphQLString))) },
+    issuers: {
+      type: new GraphQLNonNull(
+        new GraphQLList(new GraphQLNonNull(GraphQLString))
+      )
+    },
     createdAt: { type: new GraphQLNonNull(GraphQLString) },
     updatedAt: { type: GraphQLString },
   }),
