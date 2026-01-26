9faa1a719d17f0cc8fa10f455d80919310f092dc
Nonumpa <acerxp511@msn.com>
2026-01-27 01:06:54 +0800

    fix(node24): migrate .babelrc to babel.config.js
    Node 24's module resolution changes cause @babel/eslint-parser to fail
    detecting the JSON-based .babelrc config file, resulting in:
    "No Babel config file detected".
    
    Migrate from .babelrc (JSON) to babel.config.js (JS module.exports) which
    is the Babel-recommended project-wide configuration format and resolves
    the detection issue under Node 24.

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src', './test'],
+        extensions: ['.js', '.ts'],
+      },
+    ],
+  ],
+};
